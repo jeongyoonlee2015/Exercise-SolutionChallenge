92025c230140471f81c2acc1dc20ad0679f111d2
jeongyoonlee2015 <happyjoy2005@gmail.com>
2020-03-19 22:24:09 +0900

    test init

---
+++ Ex-for-soultionchallenge/bin/main.dart
@@ -0,0 +1,5 @@
+import 'package:Ex_for_soultionchallenge/Ex_for_soultionchallenge.dart' as Ex_for_soultionchallenge;
+
+void main(List<String> arguments) {
+  print('Hello world Solution Challenge: ${Ex_for_soultionchallenge.calculate()}!');
+}
